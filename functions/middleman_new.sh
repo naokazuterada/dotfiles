118f65b6888275ddfec2be4027734e990835bafd
Naokazu Terada <naokazu.terada@gmail.com>
2015-04-17 12:21:24 +0900

    cleanup

--- functions/middleman_new.sh
+++ functions/middleman_new.sh
@@ -9,21 +9,16 @@ app_name=$1
 if [ -z $app_name ]; then
   echo "usage: middleman_new [app_name]"
   echo "[app_name]を指定して下さい"
+elif [ -d $app_name ]; then
+  echo "[ERROR] ディレクトリ[$app_name]が既に存在します"
 else
-  if [ -d $app_name ]; then
-    echo "[ERROR] ディレクトリ[$app_name]が既に存在します"
-  else
 
-    git clone git@github.com:naokazuterada/middleman-template.git $app_name
-    cd $app_name
+  git clone git@github.com:naokazuterada/middleman-template.git $app_name
+  cd $app_name
 
-    git submodule init
-    git submodule update
+  git submodule init
+  git submodule update
 
-    bundle install --path=vendor/bundle --binstubs=vendor/bin
+  bundle install --path=vendor/bundle --binstubs=vendor/bin
 
-  fi
 fi
-
-
-
