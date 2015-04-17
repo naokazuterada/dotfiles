a461df0c687def109684c4bb2c747b94a70b4157
Naokazu Terada <naokazu.terada@gmail.com>
2015-04-17 12:15:27 +0900

    add command middleman_new

---
+++ functions/middleman_new.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# --------------------------------------
+# 目的
+# - middleman-templateをcheckoutして、新規サイトをサクッと作り始める
+# --------------------------------------
+
+app_name=$1
+
+if [ -z $app_name ]; then
+  echo "usage: middleman_new [app_name]"
+  echo "[app_name]を指定して下さい"
+else
+  if [ -d $app_name ]; then
+    echo "[ERROR] ディレクトリ[$app_name]が既に存在します"
+  else
+
+    git clone git@github.com:naokazuterada/middleman-template.git $app_name
+    cd $app_name
+
+    git submodule init
+    git submodule update
+
+    bundle install --path=vendor/bundle --binstubs=vendor/bin
+
+  fi
+fi
+
+
+
